dea42ce784caf5d9e87a67b816f2fe28928140c4
Juan Bohorquez Marcelo <juan.bohorquezm@perficient.com>
2024-01-22 21:41:01 -0500

    beginning serialization of input payload using google maps api example unit

--- Demo/src/test/java/DemoProject/OAUthTest.java
+++ Demo/src/test/java/DemoProject/OAUthTest.java
@@ -1,14 +1,22 @@
 package DemoProject;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 public class OAUthTest {
 
     public static void main(String[] args){
-        String response = given().formParams("cliente_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+        String response = given().formParams("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
             .formParams("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
             .formParams("grant_type", "client_credentials")
             .formParams("scope", "trust")
             .when().log().all()
             .post("https://rahulshettyacademy.com/oauthapi/oauth2/resourceOwner/token").asString();
-        System.out.println(response);
+        System.out.println("Response 1---> " + response);
+        JsonPath jsonPath = new JsonPath(response);
+        String accessToken = jsonPath.getString("access_token");
+
+        String response2 = given().queryParams("access_token", accessToken)
+            .when().log().all().get("https://rahulshettyacademy.com/oauthapi/getCourseDetails\n").asString();
+        System.out.println("Response 2---> " + response2);
     }
 }
